6d8076dde10f243ba3756ef1f27766f868b20f02
kair_003 <kairi.satellite@gmail.com>
2020-03-01 02:18:27 +0900

    change

--- main/__init__.py
+++ main/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-import main.views
+from main import views
--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
-import flask
 from main import app
 
 @app.route('/')
 def top_page():
-    return 'あ'
+    return 'Hello, World!'
